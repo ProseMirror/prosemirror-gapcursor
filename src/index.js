abaa1dbf044ff8adfc0f84cba152ec43d9b57725
Christopher Lenz <cmlenz@gmail.com>
2019-06-24 12:39:48 +0200

    Don't create gap cursor when the view is not editable. (#6)
    FIX: Do not show a gap cursor when the view isn't editable.

--- src/index.js
+++ src/index.js
@@ -54,6 +54,7 @@ function arrow(axis, dir) {
 }
 
 function handleClick(view, pos, event) {
+  if (view.someProp("editable", editable => editable(view.state) === false)) return false
   let $pos = view.state.doc.resolve(pos)
   if (!GapCursor.valid($pos)) return false
   let {inside} = view.posAtCoords({left: event.clientX, top: event.clientY})
